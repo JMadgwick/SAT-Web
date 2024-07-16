7797a24dda3ee57c107b66f71203d75f134f9b10
JMadgwick <32958136+JMadgwick@users.noreply.github.com>
2024-07-16 20:32:47 +0100

    Create basic app layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,102 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
 </script>
-
+<header></header>
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <section class="panels">
+    <!-- Left Hand Side -->
+    <div class="left-boxes">
+      <div style="border: 5px solid red;">
+        <div id="left-top">
+          <div style="border: 2px solid yellow;align-items: center;display: flex;justify-content: center"><Counter /></div>
+          <div style="border: 2px solid blue;text-align: center;"><h3>Example SAT problems: TBC</h3></div>
+        </div>
+      </div>
+      <div style="border: 5px solid green;">
+        <div id="left-mid">
+          <div style="border: 2px solid yellow;"><h3>DIMACS CNF Input:</h3><textarea style="width: 95%;height: 65%;"></textarea></div>
+          <div style="border: 2px solid blue;"><h3>SAT instance in mathematical notation:</h3><div style="width: 95%;height: 65%;border: 2px solid black;"></div></div>
+        </div>
+      </div>
+      <div style="border: 5px solid blue;"></div>
+    </div>
+    <!-- Right Hand Side -->
+    <div>
+      <div class="right-boxes">
+        <div id="right-top">
+          <div><h2>SAT solving log</h2></div>
+          <div style="text-align: right;"><a href="https://example.com" target="_blank" rel="noreferrer">User instruction manual</a></div>
+        </div>
+        <div class="output-box-container" style="background-color: aqua;">
+          <textarea class="output-box" id="sat-log" readonly value="tbc" />                    
+        </div>
+        <div>
+          <Counter /><Counter />
+          <h3>Learnt Clauses</h3>
+        </div>
+        <div class="output-box-container" style="background-color: red;">
+          <textarea class="output-box" id="learnt-clauses" readonly value="tbc" />
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
+<footer>&COPY; 2024</footer>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
+  /* Todo: Somehow use grid and flex thing to start from scratch? */
+  /* Setting 95vh on ".panels" with everything empty might be a solution, or 100vh on body or smthing? */
+  footer {
     color: #888;
+    text-align: center;
+  }
+  textarea {
+    font-size: large;
+    resize: none;
+    overflow-y: scroll;
+  }
+  .output-box {
+    width: 100%;
+    margin: 0.5em;
+  }
+  .output-box-container {
+    display: flex;
+  }
+  .panels {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    /* min-height: 95vh; */
+    /* height: 100%; */
+    /* gap: 4px; */
+    /* align-items: center;
+    display: flex; */
+  }
+  #right-top {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    /* gap: 4px; */
+  }
+  .right-boxes {
+    display: grid;
+    grid-template-rows: 4em 30em 6em 30em;/* 1fr 4fr 1fr 4fr; */
+  }
+  .left-boxes {
+    display: grid;
+    grid-template-rows: 1fr 4fr 8fr;/* 4em 30em 30em */
+    border: 5px solid chocolate;
+  }
+  #left-top {
+    display: grid;
+    grid-template-columns: 2fr 6fr;
+    border: 3px solid green;
+    height: 100%;
+    box-sizing: border-box;/* required for proper nesting */
+  }
+  #left-mid {
+    display: grid;
+    grid-template-columns: 3fr 5fr;
+    border: 3px solid orchid;
+    height: 100%;
+    box-sizing: border-box;/* required for proper nesting */
   }
 </style>
